9ad83f7bc357d5130065508081b9f68afc299b05
fneves-datalex <fabio.neves@datalex.com>
2013-11-06 14:45:02 +0000

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InTouch::Application.config.secret_key_base = 'eb5b40bda61b9a41025146417f727fc9d50f83858e8a2531b9db4e84944421389808f64018e0b8ac3bc395893047068fca1c70b06870be3d4d4953fd491e3452'
